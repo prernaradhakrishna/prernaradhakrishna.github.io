556ed8f2059219898e3feae270ef744c9334ce5a
prernaradhakrishna <prerna.radhakrishna@gmail.com>
2026-04-11 15:30:54 +0000

    Deploying to gh-pages from @ prernaradhakrishna/prernaradhakrishna.github.io@ff87243dc6efda294118a0cbe97516c77921dee0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,7 +64,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+          section: "News",},{id: "projects-a-priori-power-simulation",
+          title: 'A Priori Power Simulation',
+          description: "Power simulation for the information load experiment",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/power_simulation/";
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
